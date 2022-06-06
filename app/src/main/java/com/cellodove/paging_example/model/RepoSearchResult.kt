b9ed9cbf1831ba28f210a074c344c0a84b5d4685
wntjd200@naver.com <wntjd200@naver.com>
2022-06-06 22:20:23 +0900

    .

---
+++ app/src/main/java/com/cellodove/paging_example/model/RepoSearchResult.kt
@@ -0,0 +1,8 @@
+package com.cellodove.paging_example.model
+
+import java.lang.Exception
+
+sealed class RepoSearchResult {
+    data class Success(val data: List<Repo>) : RepoSearchResult()
+    data class Error(val error: Exception) : RepoSearchResult()
+}
