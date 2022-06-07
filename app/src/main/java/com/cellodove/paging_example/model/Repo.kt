b3e92bc3afb79c75ee643330116bd41894cfa40b
jusung.kim <wntjd200@naver.com>
2022-06-07 15:40:51 +0900

    모델 수정

--- app/src/main/java/com/cellodove/paging_example/model/Repo.kt
+++ app/src/main/java/com/cellodove/paging_example/model/Repo.kt
@@ -4,9 +4,8 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-@Entity(tableName = "repos")
 data class Repo(
-    @PrimaryKey @field:SerializedName("id") val id: Long,
+    @field:SerializedName("id") val id: Long,
     @field:SerializedName("name") val name: String,
     @field:SerializedName("full_name") val fullName: String,
     @field:SerializedName("description") val description: String?,
