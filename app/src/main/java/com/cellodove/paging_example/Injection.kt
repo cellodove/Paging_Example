8e95a991ca6082ca003757f48e05740f4ca894a7
jusung.kim <wntjd200@naver.com>
2022-06-07 09:35:25 +0900

    완성

--- app/src/main/java/com/cellodove/paging_example/Injection.kt
+++ app/src/main/java/com/cellodove/paging_example/Injection.kt
@@ -1,4 +1,18 @@
 package com.cellodove.paging_example
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.savedstate.SavedStateRegistryOwner
+import com.cellodove.paging_example.api.GithubService
+import com.cellodove.paging_example.data.GithubRepository
+import com.cellodove.paging_example.ui.ViewModelFactory
+
 object Injection {
+
+    private fun provideGithubRepository(): GithubRepository {
+        return GithubRepository(GithubService.create())
+    }
+
+    fun provideViewModelFactory(owner: SavedStateRegistryOwner): ViewModelProvider.Factory {
+        return ViewModelFactory(owner, provideGithubRepository())
+    }
 }
--- app/src/main/java/com/cellodove/paging_example/api/GithubService.kt
+++ app/src/main/java/com/cellodove/paging_example/api/GithubService.kt
@@ -10,7 +10,7 @@ import retrofit2.http.Query
 const val IN_QUALIFIER = "in:name,description"
 
 interface GithubService {
-    @GET
+    @GET("search/repositories?sort=stars")
     suspend fun searchRepos(
         @Query("q")query: String, @Query("page") page : Int, @Query("per_page") itemsPerPage: Int
     ) : RepoSearchResponse
